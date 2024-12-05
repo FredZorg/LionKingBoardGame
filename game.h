b1d9cafe1057f7a23f2d052964c8af4468187c34
FredZorgdrager <zorgdrager_frederick@svvsd.org>
2024-12-05 15:37:36 -0700

    added the txt files and started building the class

---
+++ game.h
@@ -0,0 +1,18 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game {
+    public:
+        //use the characters.txt file to replace the currentCharacters.txt file after the game is over
+        void resetCharacters();
+        //when a character is selected remove it from the currentCharacters.txt
+        void removeSelectedCharacter();
+
+
+    private:
+
+
+};
+
+
+#endif
